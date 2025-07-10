b413c6e696c983d63638cb9a5a477b9f611e30e9
Ulaev Danila <ulaevdanila63@gmail.com>
2025-07-10 19:33:25 +0200

    Added FileDownloader class

---
+++ AsyncFileDownloader/FileDownloader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FileDownloader;
+
+public class FileDownloader(string url)
+{
+    private string _destinationPath = "D:\\Programming\\C#\\Trainee\\AsyncFileDownloader\\AsyncFileDownloader\\Result";
+    private string _url = url;
+
+    private HttpClient _httpClient = new HttpClient;
+
+    public async Task DownloadFile(CancelationToken cancelationToken, IProgress<int> progress)
+    {
+        try
+        {
+            var content = _httpClient.GetByteArrayAsync(_url, cancelationToken);
+            await File.WriteAllBytesAsync(_destinationPath, content);
+            progress?.report(100);
+        }
+        catch (TaskCanceledException e)
+        {
+            Console.WriteLine("Произошёл сбой");
+            throw;
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("Введён некорретный url");
+            throw;
+        }
+    }
+}
